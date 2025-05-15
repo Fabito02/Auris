f365a99e0e789c72bdfd5f49b15057bb53fe0380
Fabito02 <fabianojuniorlimaba2@gmail.com>
2025-05-15 13:31:23 -0300

    fix: atualizar NavbarComponent para gerenciar estados de links ativos com base na rota atual

--- frontend/src/components/NavbarComponent.tsx
+++ frontend/src/components/NavbarComponent.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavbarComponent.css";
 import {
   DropdownMenu,
@@ -14,6 +14,7 @@ const NavbarComponent = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [permissao, setPermissao] = useState(false);
   const lastScrollY = useRef(0);
+  const location = useLocation();
 
   const handleScroll = useCallback(() => {
     const currentScrollY = window.scrollY;
@@ -42,6 +43,33 @@ const NavbarComponent = () => {
     };
   }, [handleScroll]);
 
+  useEffect(() => {
+    document.getElementById("link-home")?.classList.remove("active");
+    document.getElementById("link-fale-conosco")?.classList.remove("active");
+    document.getElementById("link-informacoes")?.classList.remove("active");
+    document.getElementById("link-regulamento")?.classList.remove("active");
+    document.getElementById("link-gerenciar")?.classList.remove("active");
+    document.getElementById("link-acompanhamento")?.classList.remove("active");
+
+    const page = location.pathname.split("/")[1];
+    const pageAdmin = location.pathname.split("/admin/")[1];
+
+    if (page === "home") {
+      document.getElementById("link-home")?.classList.add("active");
+    } else if (page === "fale-conosco") {
+      document.getElementById("link-fale-conosco")?.classList.add("active");
+    } else if (page === "informacoes") {
+      document.getElementById("link-informacoes")?.classList.add("active");
+    } else if (page === "regulamento") {
+      document.getElementById("link-regulamento")?.classList.add("active");
+    } else if (pageAdmin === "gerenciar") {
+      document.getElementById("link-gerenciar")?.classList.add("active");
+    } else if (pageAdmin === "acompanhamento") {
+      document.getElementById("link-acompanhamento")?.classList.add("active");
+    }
+  }, [location.pathname]);
+  
+
   return (
     <nav
       className={`fixed top-[65px] left-0 w-full z-50 bg-white border-t border-b py-1.5 lg:flex justify-between mx-auto px-4 transition-transform duration-300 ease-in-out ${
@@ -50,25 +78,25 @@ const NavbarComponent = () => {
       style={{ height: "45px" }}
     >
       <div className="hidden xl:flex space-x-4 justify-center w-full">
-        <Link to="/home" className="link-navbar">
+        <Link to="/home" className="link-navbar active" id="link-home">
           HOME
         </Link>
-        <Link to="/fale-conosco" className="link-navbar">
+        <Link to="/fale-conosco" className="link-navbar" id="link-fale-conosco">
           FALE CONOSCO
         </Link>
-        <Link to="/informacoes" className="link-navbar">
+        <Link to="/informacoes" className="link-navbar" id="link-informacoes">
           INFORMAÇÕES E FAQs
         </Link>
-        <Link to="/regulamento" className="link-navbar">
+        <Link to="/regulamento" className="link-navbar" id="link-regulamento">
           POLÍTICAS E REGULAMENTOS
         </Link>
         {permissao && (
           <>
             <div className="border-r"></div>
-            <Link to="/admin/gerenciar" className="link-navbar">
+            <Link to="/admin/gerenciar" className="link-navbar" id="link-gerenciar">
               GERENCIAR
             </Link>
-            <Link to="/admin/acompanhamento" className="link-navbar">
+            <Link to="/admin/acompanhamento" className="link-navbar" id="link-acompanhamento">
               ACOMPANHAMENTO
             </Link>
           </>
